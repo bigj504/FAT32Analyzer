be6139cd56337be62cd7d6e863c17aef630fea69
bigj504 <jpgilles@uno.edu>
2019-11-26 10:03:53 -0600

    batch file edits

--- FAT32Analyzer.bat
+++ FAT32Analyzer.bat
@@ -1,7 +1,9 @@
 @echo off
-echo What is the file image path?
-set /p Name=Enter your file image path:;
-echo Your file image path is %Name% & pause
+set /p Name=Enter your path to your FAT32 image:
+set /p End=Enter your path for repaired file image include .dd extention at the end:
+echo Your file image path is %Name% 
+echo Your repaired file image path is %End% 
+echo Press any key to begin. & pause
 javac FAT32Analyzer.java
-java FAT32Analyzer %Name%
+java FAT32Analyzer %Name% %End%
 pause
